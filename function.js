273aecc08b3e7229fde311d508c2867177a0cd86
vicki <vicki.li1109@icloud.com>
2017-03-22 21:47:18 +0800

    函数式编程2

--- function.js
+++ function.js
@@ -15,6 +15,7 @@ console.log(isNaN(NaN));
 var isNotNaN = reverse(isNaN);
 console.log(isNotNaN(NaN));
 
+/**映射函数*/
 function map(func,array)
 {
 	var result = [];
@@ -32,3 +33,35 @@ function double(value)
 
 var a = [1,2,3,4,0];
 console.log(map(double,a));
+
+/**规约函数，操作符应用*/
+function reduce(combine,base,array)
+{
+	forEach(array,function(value)
+	{
+		base = combine(base,value);
+	});
+	return base;
+}
+
+var ops = {
+	"+":function(x,y){return x+y;},
+	"-":function(x,y){return x-y;},
+	"*":function(x,y){return x*y;},
+	"/":function(x,y){return x/y;}
+};
+
+function operation(op,array)
+{
+	if(op in ops)
+	{
+		return reduce(ops[op],0,array);
+	}
+}
+
+var a=[1,2,3,4,0]; 
+
+console.log(operation("+",a));
+console.log(operation("/",a));
+
+
